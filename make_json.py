3fc486fc76a1993e8c9101a274068f41d595f278
cercil0605 <kuamaichi2223@gmail.com>
2022-12-12 09:34:59 +0900

    make file for bluetooth

--- make_json.py
+++ make_json.py
@@ -1,19 +1,14 @@
 import json
 import datetime 
 import bluepy
-#make json data format
-#[
-#{"time": time(str), "address": address(bluetooth)},
-#{"time": time2(str), "address": address2(bluetooth)}
-#]
 
-scanner = bluepy.btle.Scanner(0)
-devices=scanner.scan(3)
-f=open("samplefile.txt","a")
+scanner = bluepy.btle.Scanner(0) #init
+devices=scanner.scan(3) #scan 3sec
+f=open("samplefile.txt","a") #begin file write 上書きmode
 f.write("======================================================\n")
-f.write("time"+" "+"address\n")
+f.write("time"+"                       "+"address\n") #時間 アドレス 必要であればRSSI（電波強度）
 
-for device in devices:
+for device in devices: #3s検知開始
   str_dt=str(datetime.datetime.now()).replace(" ","_").replace(":", "-")
   #print('address : %s' % device.addr)
   #print('addrType: %s' % device.addrType)
@@ -22,3 +17,5 @@ for device in devices:
   f.write("{} {}\n".format(str_dt,str(device.addr)))
   #for (adtype, desc, value) in device.getScanData():
     #print(' (%3s) %s : %s ' % (adtype, desc, value))
+
+f.close()
